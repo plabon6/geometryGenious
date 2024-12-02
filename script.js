40a36aa3b17565922c5159247b44bfe1df29055f
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2024-12-02 20:39:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const a = 20;
+const b = 20;
+// const result = document.getElementById("result");
+
+function multi() {
+    return a*b;
+}
+
+console.log(multi(a,b));
+
+
+// result.innerHTML = `<p>Tringle Result is ${multi}</p>`
+
